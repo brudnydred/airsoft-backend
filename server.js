79c2698fce08ccaf541d8b83eb45e017c342f4bc
czarnypasti <49869722+czarnypasti@users.noreply.github.com>
2020-08-18 16:00:35 +0200

    3

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path')
 require('dotenv').config()
 
 const app = express()
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
