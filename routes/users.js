24b5a5cd111ce6b1e8501b3592c4d1c829a0b7bc
czarnypasti <49869722+czarnypasti@users.noreply.github.com>
2020-08-18 19:12:44 +0200

    add new routes in users.js

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const User = require('./../models/user.model')
 router.get('/', async (req, res) => {
   try {
     const users = await User.find()
+
     res.json(users)
   } catch (err) {
     res.json(`Error: ${err}`)
@@ -12,29 +13,38 @@ router.get('/', async (req, res) => {
 
 router.post('/add', async (req, res) => {
   const { username, password, email } = req.body
-  
-  const currentDate = new Date()
-  const createdAt = currentDate
-  const lastActiveAt = currentDate
-
-  const newUser = new User({
-    username,
-    password,
-    email,
-    createdAt,
-    lastActiveAt,
-  })
 
   try {
-    await newUser.save()
-    res.json('User added')
-  } catch (err) {
-    res.json(`Error: ${err}`)
+    if ((await User.find({ username: username })).length || (await User.find({ email: email })).length) {
+      res.json('This username or email is taken.')
+    } else {
+      const currentDate = new Date()
+      const createdAt = currentDate
+      const lastActiveAt = currentDate
+
+      const newUser = new User({
+        username,
+        password,
+        email,
+        createdAt,
+        lastActiveAt,
+      })
+    
+      try {
+        await newUser.save()
+        res.json('User added')
+      } catch (err) {
+        res.json(`Error: ${err}`)
+      }
+    }
+  } catch (error) {
+    
   }
 })
 
 router.delete('/delete/:id', async (req, res) => {
   const { id } = req.params
+
   try {
     await User.deleteOne({ _id: id })
     res.json('User deleted')
@@ -61,12 +71,89 @@ router.put('/update/:id', async (req, res) => {
   }
 })
 
-router.put('/logout', (req, res) => {
+router.put('/logout/:id', async (req, res) => {
+  const { id } = req.params
 
+  try {
+    await User.updateOne({ _id: id }, {
+      $set: {
+        lastActiveAt: new Date()
+      }
+    })
+    res.json('User logged out')
+  } catch (err) {
+    res.json(`Error: ${err}`)    
+  }
 })
 
-router.put('/remove_friend/:id', (req, res) => {
+router.put('/add_friend/:id', async (req, res) => {
+  const { id } = req.params
+  const { friendUsername } = req.body
 
+  try {
+    const { friends } = await User.findOne({ _id: id }, 'friends -_id')
+
+    if (friends.some(friend => friend === friendUsername)) {
+      res.json(`${friendUsername} is already your friend`)
+    } else {
+      try {
+        const friendData = await User.findOne({ username: friendUsername }, '_id')
+        const userData = await User.findOne({ _id: id }, 'username -_id')
+
+        if (friendData?._id) {
+          try {
+            await User.updateOne({ _id: id }, {
+              $push: {
+                friends: friendUsername
+              }
+            })
+
+            await User.updateOne({ _id: friendData._id }, {
+              $push: {
+                friends: userData.username
+              }
+            })
+
+            res.json('Friend added')
+          } catch (err) {
+            res.json(`Error: ${err}`)
+          }
+        } else {
+          res.json('User not found')
+        }
+      } catch (err) {
+        res.json(`Error: ${err}`)  
+      }
+    }
+  } catch (err) {
+    res.json(`Error: ${err}`)
+  }
+})
+
+router.put('/remove_friend/:id', async (req, res) => {
+  const { id } = req.params
+  const { friendUsername } = req.body
+
+  try {
+    const { _id: friendId } = await User.findOne({ username: friendUsername }, '_id')
+    const { username: userUsername } = await User.findOne({ _id: id }, 'username -_id')
+
+    await User.updateOne({ _id: id }, {
+      $pull: {
+        friends: friendUsername
+      }
+    })
+
+    await User.updateOne({ _id: friendId }, {
+      $pull: {
+        friends: userUsername
+      }
+    })
+
+    res.json('Friend removed')
+  } catch (err) {
+    res.json(`Error: ${err}`)
+  }
 })
 
 module.exports = router
