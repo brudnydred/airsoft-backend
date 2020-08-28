21d752626359975c395cc5fdd0aa6fc09ffdf11b
czarnypasti <49869722+czarnypasti@users.noreply.github.com>
2020-08-28 12:46:00 +0200

    test

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -3,8 +3,8 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
   const token = req.header('auth-token')
 
-  console.log()
-
+  console.log(token)
+  
   if (!token) return res.status(401).json({ success: false, error: "Access Denied"})
 
   try {
